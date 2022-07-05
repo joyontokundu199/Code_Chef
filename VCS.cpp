2951b756399458e65b36b33e099069c7d920326a
JOYONTO KUNDU <87633431+joyontokundu199@users.noreply.github.com>
2022-07-05 10:04:29 +0530

    Add files via upload

---
+++ VCS.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int N,M,K;
+	    cin>>N>>M>>K;
+	    vector<int> v(M),v1(K);
+	    map<int,int> m;
+	    for(int i=0;i<M;i++)
+	    {
+	        int x;
+	        cin>>x;
+	        v.push_back(x);
+	       
+	    }
+	    for(int i=0;i<K;i++)
+	    {
+	     
+	        int y;
+	        cin>>y;
+	        v1.push_back(y);
+	        
+	    }
+	    for(int i=0;i<v.size();i++)
+	    {
+	        m[v[i]]++;
+	    }
+	    for(int i=0;i<v1.size();i++)
+	    {
+	        m[v1[i]]++;
+	    }
+	    int count=0,count1=0;
+	    for(int i=0;i<=N;i++)
+	    {
+	        if(m[i]==2)
+	        {
+	            count++;
+	        }
+	        else if(m[i]==0)
+	        {
+	            count1++;
+	        }
+	    }
+	    cout<<count<<" "<<count1<<endl;
+	}
+	return 0;
+}
